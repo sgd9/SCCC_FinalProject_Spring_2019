d8f76afb01f4b6a06fd5a31957952ef5c3dfa31a
sgd9 <46510859+sgd9@users.noreply.github.com>
2019-06-06 03:58:29 -0700

    Add files via upload

--- dogbreed/dogbreedapp/views.py
+++ dogbreed/dogbreedapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import DogBreed, Dog, Review
 
 # Create your views here.
 def index (request):
     return render(request, 'dogbreedapp/index.html')
+
+def gettypes(request):
+    dogbreed_list=DogBreed.objects.all()
+    return render(request, 'dogbreedapp/types.html' ,{'dogbreed_list' : dogbreed_list})
